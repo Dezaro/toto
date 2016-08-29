7aa2efa085cc0ac4966b9330c9a1543328935619
Dezaro <Dezaro@192.168.2.64>
2016-08-29 10:24:00 +0300

    Bug whit range exception is fixed

--- Toto.php
+++ Toto.php
@@ -38,17 +38,15 @@ function rec($arr, $n) {
     }
     $j++;
   }
-//  var_dump($arr);
+
   for($i = 0; $i < $count; $i++) {
     if($i === ($count - 1)) {
       if($arr[$i] === $arr[0]) {
-//        var_dump($n);
-        $arr[$i] += rand(1, $n);
+        $arr[$i] = rand(1, $n);
         return rec($arr, $n);
       }
     } elseif($arr[$i] === $arr[$i + 1]) {
-//      var_dump($n);
-      $arr[$i] += rand(1, $n);
+      $arr[$i] = rand(1, $n);
       return rec($arr, $n);
     }
   }
@@ -58,37 +56,12 @@ function rec($arr, $n) {
 function gen($n, $k) {
   $rands = array();
   for($i = 1; $i <= $k; ++$i) {
-//    var_dump($n);
     $rands[] = rand(1, $n);
   }
   $rands = rec($rands, $n);
   asort($rands);
   return implode(' ', $rands);
-//  return $rands;
 }
-
-//$use = comb(35, 5);
-//print 'Chance to win of 5/35 is 1 to ' . $use . ' (1/' . $use . ') !<br>';
-//
-//$use = comb(42, 6);
-//print 'Chance to win of 6/42 is 1 to ' . $use . ' (1/' . $use . ') !<br>';
-//
-//$use = comb(47, 6);
-//print 'Chance to win of 6/47 is 1 to ' . $use . ' (1/' . $use . ') !<br>';
-//
-//$use = comb(49, 6);
-//print 'Chance to win of 6/49 is 1 to ' . $use . ' (1/' . $use . ') !<br>';
-//
-//$random = gen(35, 5);
-//print 'Random generate numbers for 5/35: <b>' . $random . '</b> !<br>';
-//
-//$random = gen(42, 6);
-//print 'Random generate numbers for 6/42: <b>' . $random . '</b> !<br>';
-//
-//$random = gen(47, 6);
-//print 'Random generate numbers for 6/47: <b>' . $random . '</b> !<br>';
-//$random = gen(49, 6);
-//print 'Random generate numbers for 6/49: <b>' . $random . '</b> !<br>';
 
 $result = array();
 
@@ -99,10 +72,9 @@ for($i = 0; $i < 4; ++$i) {
   $result['use35'][$i][] = gen(49, 6);
 }
 
-$result['chance35'] = comb(35, 5); 
-$result['chance42'] = comb(42, 6); 
-$result['chance47'] = comb(47, 6); 
-$result['chance49'] = comb(49, 6); 
+//$result['chance35'] = comb(35, 5); 
+//$result['chance42'] = comb(42, 6); 
+//$result['chance47'] = comb(47, 6); 
+//$result['chance49'] = comb(49, 6); 
 
-//print json_encode($random);
 print json_encode($result);
